01e2153af69c4160d2cdba5687f23831354ea937
wiston jose quintana osorio <wistonjosequintanaosorio@hotmail.com>
2021-12-05 13:39:24 -0600

    Tu marido bueno si

--- TeatroSistema/Program.cs
+++ TeatroSistema/Program.cs
@@ -11,13 +11,13 @@ namespace TeatroSistema
         /// <summary>
         /// Punto de entrada principal para la aplicación.
         /// </summary>
-        [STAThread]
+ 
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
             Application.Run(new FrmMain()) ;
-        }
+        }  
     }
 }
